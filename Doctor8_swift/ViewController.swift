2af3425704849758e5ea6f2cb30746a1e8b427b8
huihui <hui1107464497@163.com>
2017-05-15 10:13:42 +0800

    Initial Commit

---
+++ Doctor8_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doctor8_swift
+//
+//  Created by ihaoyisheng on 2017/5/15.
+//  Copyright © 2017年 Rabbij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
